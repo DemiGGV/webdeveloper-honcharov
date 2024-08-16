5ce7024e40ee656f8093c10894ca284d53f815e5
Heorhii Honcharov <0019@ukr.net>
2024-08-16 12:08:18 +0200

    1

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/webdeveloper-honcharov">
+    <BrowserRouter basename="/webdeveloper-honcharov/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,

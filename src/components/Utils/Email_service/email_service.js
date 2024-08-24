44b783ad8a77c89a27645c91bb415dbbf66dc47a
Heorhii Honcharov <0019@ukr.net>
2024-08-24 16:15:38 +0200

    1

--- src/components/Utils/Email_service/email_service.js
+++ src/components/Utils/Email_service/email_service.js
@@ -1,9 +1,13 @@
 import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
 
-const publicKey = import.meta.env.VITE_PUBLIC_KEY;
-const serviceId = import.meta.env.VITE_SERVICE_ID;
-const templateId = import.meta.env.VITE_TEMPLATE_ID;
+// const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+// const serviceId = import.meta.env.VITE_SERVICE_ID;
+// const templateId = import.meta.env.VITE_TEMPLATE_ID;
+const publicKey = '_fc5lvapttQECnMGG';
+const serviceId = 'service_mb64pcv';
+const templateId = 'template_haov9xh';
+
 
 emailjs.init({
   publicKey: publicKey,
